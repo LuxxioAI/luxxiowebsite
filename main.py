ef5602ca6c396a32ff9d654c3c3761390797f595
novativeai <155618104+novativeai@users.noreply.github.com>
2025-03-31 11:22:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,48 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import replicate
-from dotenv import find_dotenv, load_dotenv
+from dotenv import load_dotenv
 
 # Finding variables
-dotenv_path = find_dotenv()
-load_dotenv(dotenv_path)
+load_dotenv()
 
 # FastAPI app
-app_fastapi = FastAPI()
+app = FastAPI()
 
 
-@app_fastapi.get("/")
+class generateRequest(BaseModel):
+    prompt: str
+    aspect_ratio: str
+
+
+
+@app.get("/home")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
 
 
-@app_fastapi.post("/api/generate-image")
-async def generate_image(prompt: str, aspect_ratio: str):
-    output = replicate.run(
-        "black-forest-labs/flux-1.1-pro",
-        input={
-            "prompt": prompt,
-            "aspect_ratio": aspect_ratio,
+@app.post("/generate")
+async def generate_image(
+    request: generateRequest,
+):
+
+    input = {
+            "prompt": request.prompt,
+            "aspect_ratio": request.aspect_ratio,
             "output_format": "png",
             "output_quality": 80,
             "safety_tolerance": 2,
             "prompt_upsampling": True
-        }
-    )
-
+    }
     try:
-
         # Send POST request (replace with actual request method if needed)
         output = replicate.run(
-            "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+            "black-forest-labs/flux-1.1-pro",
             input=input,
         )
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error generating image: {e}")
 
     # Handle response (replace with logic to process actual response data)
     return {"img": output}
-
-
-if __name__ == "__main__":
-    # FastAPI on port 8000 (modify as needed)
-    import uvicorn
-
-    uvicorn.run(app_fastapi, host="0.0.0.0", port=8000)
